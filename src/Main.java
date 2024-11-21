2cef50d5bf1ac72ce249f21be7fb9168a7ccfe73
sahar <sahar.ayazian@gmail.com>
2024-11-21 10:05:46 -0800

    Finished Inset, added Type

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,78 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static String filePath = "/Users/sahar/IdeaProjects/LineEditor/src/file.txt";
+    public static int currentLineNumber = 3;
+    public static String[] fileLines = new String[100];
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readFileLines(filePath);
         boolean continued = true;
         showOptions();
         while (continued) {
-            String userInput = readInputs();
+            String userInput = readInput();
             continued = runAction(userInput);
         }
     }
 
-    private static boolean runAction(String userInput) {
+    private static void readFileLines(String filePath) throws FileNotFoundException {
+        File theFile = new File(filePath);
+        Scanner input = new Scanner(theFile);
+        for (int i = 0; i < 100; i++) {
+            if (input.hasNextLine()) fileLines[i] = input.nextLine();
+        }
+    }
+
+    private static boolean runAction(String userInput) throws FileNotFoundException {
         char currentCommand = getCurrentCommand(userInput);
         boolean continued = true;
 
         switch (currentCommand) {
             case 's':
-                substituteCmd(userInput);
+                trySubstituteCommand(userInput);
                 break;
             case 'c':
-                copyCmd(userInput);
+                tryCopyCommand(userInput);
                 break;
             case 'l':
-                locateCmd(userInput);
+                tryLocateCommand(userInput);
                 break;
             case 'd':
-                deleteCmd(userInput);
+                tryDeleteCommand(userInput);
                 break;
             case 'm':
-                moveCmd(userInput);
+                tryMoveCommand(userInput);
                 break;
             case 't':
-                typeCmd(userInput);
+                tryTypeCommand(userInput);
                 break;
             case 'p':
-                pasteCmd();
+                tryPasteCommand();
                 break;
             case 'i':
-                insertCmd(userInput);
+                tryInsertCommand(userInput);
                 break;
             case 'r':
-                replaceCmd(userInput);
+                tryReplaceCommand(userInput);
                 break;
             case 'q':
-                quitCmd();
+                tryQuitCommand();
                 continued = false;
+                break;
+            default:
+                System.out.println("Invalid choice.");
                 break;
         }
         return continued;
     }
 
-
     private static char getCurrentCommand(String userInput) {
         return userInput.toLowerCase().charAt(0);
-    }
-
-    public static String readInputs() {
-        String refString = "scldmtpirq";
-        boolean choiceIsNotValid = true;
-        String[] inputs = new String[]{};
-        String userInput = "";
-
-        while (choiceIsNotValid) {
-            userInput = readInput();
-            char inputChar = userInput.toLowerCase().charAt(0);
-            if (refString.indexOf(inputChar) < 0) {
-                System.out.println("Invalid choice.");
-            } else {
-                choiceIsNotValid = false;
-            }
-        }
-        return userInput;
     }
 
     public static String readInput() {
@@ -82,104 +83,166 @@ public class Main {
 
     private static void showOptions() {
         System.out.println("Line Editor");
-        System.out.println("Substitute/oldstring/newstring/\t\tType #");
-        System.out.println("Copy #\t\t\t\t\t\t\t\tPaste");
-        System.out.println("Locate/string/\t\t\t\t\t\tInsert #");
-        System.out.println("Delete #\t\t\t\t\t\t\tReplace #");
-        System.out.println("Move #\t\t\t\t\t\t\t\tQuit");
-
+        System.out.println("Substitute/old-string/new-string/\t\t\tType #");
+        System.out.println("Copy #\t\t\t\t\t\t\t\t\t\tPaste");
+        System.out.println("Locate/string/\t\t\t\t\t\t\t\tInsert #");
+        System.out.println("Delete #\t\t\t\t\t\t\t\t\tReplace #");
+        System.out.println("Move #\t\t\t\t\t\t\t\t\t\tQuit");
     }
 
-    public static void substituteCmd(String input) {
-        System.out.println("you have chosen \"Substitue\".");
+    public static void trySubstituteCommand(String input) {
         String[] inputs = input.split("/");
-        if (inputs.length != 3) {
-            System.out.println("Invalid input for locate.");
-        } else {
-            System.out.println("you have chosen \"Locate\" with inputs: \"" + inputs[1] + "\" & \"" + inputs[2] + "\".");
-        }
+        if ((inputs.length != 3) || (input.charAt(input.length() - 1) != '/'))
+            System.out.println("Invalid input for substitute.");
+        else runSubstituteCommand(inputs[1], inputs[2]);
     }
 
-    private static void copyCmd(String input) {
+    private static void runSubstituteCommand(String substituted, String replacing) {
+        System.out.println("You have chosen \"substitute\" with inputs: \"" + substituted + "\" & \"" + replacing + "\".");
+    }
+
+    private static void tryCopyCommand(String input) {
         int number = getLineNumber(input);
-        if (number > 0) {
-            System.out.println("you have chosen to \"Copy\" line #" + number + ".");
-        }
+        if (number > 0) runCopyCommand(number);
+        else System.out.println("Invalid input for \"Copy\".");
     }
 
-    public static void locateCmd(String input) {
+    private static void runCopyCommand(int number) {
+        System.out.println("You have chosen to \"Copy\" line #" + number + ".");
+    }
+
+    public static void tryLocateCommand(String input) {
         String[] inputs = input.split("/");
-        if (inputs.length != 2) {
-            System.out.println("Invalid input for locate.");
-        } else {
-            System.out.println("you have chosen \"Locate\" with input: \"" + inputs[1] + "\".");
-        }
+        boolean hasTwoStrings = inputs.length == 2;
+        boolean hasTerminalSlash = input.charAt(input.length() - 1) == '/';
+        if (!hasTwoStrings || !hasTerminalSlash) System.out.println("Invalid input for locate.");
+        else runLocateCommand(inputs[1]);
     }
 
-    public static void deleteCmd(String input) {
+    private static void runLocateCommand(String input) {
+        System.out.println("You have chosen \"Locate\" with input: \"" + input + "\".");
+    }
+
+    public static void tryDeleteCommand(String input) {
         int number = getLineNumber(input);
-        if (number > 0) {
-            System.out.println("You have chosen to \"Delete\" line #" + number + ".");
-        }
+        if (number > 0) runDeleteCommand(number);
+        else System.out.println("Invalid input for \"Delete\".");
     }
 
-    public static void moveCmd(String input) {
+    private static void runDeleteCommand(int number) {
+        System.out.println("You have chosen to \"Delete\" line #" + number + ".");
+    }
+
+    public static void tryMoveCommand(String input) {
         int number = getLineNumber(input);
-        if (number > 0) {
-            System.out.println("You have chosen to \"Move\" line #" + number + ".");
+        if (number > 0) runMoveCommand(number);
+        else System.out.println("Invalid input for \"Move\".");
+    }
+
+    private static void runMoveCommand(int number) {
+        currentLineNumber = currentLineNumber + number;
+        if (!(fileLines[currentLineNumber - 1] == null)) System.out.println(fileLines[currentLineNumber - 1]);
+        else {
+            System.out.println("The line #" + currentLineNumber + " is empty.");
+            System.out.println("Current line is set to the number before running \"Type\" command.");
+            currentLineNumber = currentLineNumber - number;
         }
     }
 
-    public static void typeCmd(String input) {
+    public static void tryTypeCommand(String input) {
         int number = getLineNumber(input);
-        if (number > 0) {
-            System.out.println("You have chosen to \"Type\" line #" + number + ".");
+        if (number > 0) runTypeCommand(number);
+        else System.out.println("Invalid input for \"Type\".");
+    }
+
+    private static void runTypeCommand(int number) {
+        for (int i = currentLineNumber - 1; i < currentLineNumber + number - 1; i++) {
+            if (!(fileLines[i] == null)) System.out.println(fileLines[i]);
         }
     }
 
-    public static void pasteCmd() {
+    public static void tryPasteCommand() {
         System.out.println("You have chosen \"Paste\".");
 
     }
 
-    public static void insertCmd(String input) {
+    public static void tryInsertCommand(String input) {
         int number = getLineNumber(input);
-        if (number > 0) {
-            System.out.println("You have chosen to \"Insert\" line #" + number + ".");
+        if (number > 0) runInsertCommand(number);
+        else System.out.println("Invalid input for \"Insert\".");
+    }
+
+    private static void runInsertCommand(int number) {
+        String[] newLines = enterInsertLines(number);
+        insertTheNewLines(newLines);
+        currentLineNumber = currentLineNumber + number;
+        printTheFile();
+    }
+
+    private static void printTheFile() {
+        for (int i = 0; i < 100; i++) {
+            if (!(fileLines[i] == null)) System.out.println(fileLines[i]);
         }
     }
 
-    public static void replaceCmd(String input) {
-        int number = getLineNumber(input);
-        if (number > 0) {
-            System.out.println("You have chosen to \"Replace\" line #" + number + ".");
-        }
+    private static void insertTheNewLines(String[] newLines) {
+        insertTheNewLines(newLines, fileLines, currentLineNumber);
     }
 
-    public static void quitCmd() {
+    private static void insertTheNewLines(String[] newLines, String[] fileLines, int currentLineNumber) {
+        String[] temp = new String[fileLines.length - currentLineNumber];
+        for (int i = 0; i < (100 - currentLineNumber); i++) temp[i] = fileLines[currentLineNumber + i];
+        for (int i = 0; i < newLines.length; i++) fileLines[currentLineNumber + i] = newLines[i];
+        for (int i = 0; i < (100 - currentLineNumber - newLines.length); i++)
+            fileLines[currentLineNumber + newLines.length + i] = temp[i];
+    }
+
+
+    public static String[] enterInsertLines(int number) {
+        Scanner input = new Scanner(System.in);
+        String[] newLines = new String[number];
+        System.out.println("Please enter " + number + " lines > ");
+        for (int i = 0; i < number; i++) {
+            newLines[i] = input.nextLine();
+        }
+        System.out.println("You have entered: " + Arrays.toString(newLines));
+        return newLines;
+    }
+
+    public static void tryReplaceCommand(String input) {
+        int number = getLineNumber(input);
+        if (number > 0) runReplaceCommand(number);
+        else System.out.println("Invalid input for \"Replace\".");
+    }
+
+    private static void runReplaceCommand(int number) {
+        System.out.println("You have chosen to \"Replace\" line #" + number + ".");
+    }
+
+    public static void tryQuitCommand() throws FileNotFoundException {
         System.out.println("You have chosen \"Quit\".");
+        saveTheFile();
+    }
+
+    private static void saveTheFile() throws FileNotFoundException {
+        PrintWriter output = new PrintWriter(filePath);
+        for (int i = 0; i < 100; i++) {
+            if (!(fileLines[i] == null)) output.println(fileLines[i]);
+        }
+        output.close();
     }
 
     private static int getLineNumber(String input) {
         String[] inputs = input.split(" ");
-        if (inputs.length != 2) {
-            System.out.println("Invalid line number.");
-            return -1;
-        }
-        return isNumeric(inputs[1]);
+        if (inputs.length != 2) return -1;
+        return isNumeric(inputs[1]) ? Integer.parseInt(inputs[1]) : -1;
     }
 
-    private static int isNumeric(String input) {
-        char[] inputs = input.toCharArray();
-        String numbers = "0123456789";
-
+    public static boolean isNumeric(String input) {
         for (int i = 0; i < input.length(); i++) {
-            if (numbers.indexOf(inputs[i]) < 0) {
-                System.out.println("The line number is invalid.");
-                return -1;
-            }
+            if (input.charAt(i) < '0' || input.charAt(i) > '9') return false;
         }
-        return Integer.parseInt(input);
+        return true;
     }
 
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,50 +1,33 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
 
-    @Test
-    void readInputs() {
+//    @ParameterizedTest
+//    @CsvSource({
+//            "true,20",
+//            "true,12"
+//    })
+//    void testIsNumeric(boolean expected, String input) {
+//        assertEquals(expected, Main.isNumeric(input));
+//    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"20", "12"})
+    void testIsNumericPasses(String input) {
+        assertTrue(Main.isNumeric(input));
     }
 
-    @Test
-    void enterInputs() {
-        String theInput = Main.readInput();
-        System.out.println(theInput);
+    @ParameterizedTest
+    @ValueSource(strings = {"BADbadBAD", "??"})
+    void testIsNumericFails(String input) {
+        assertFalse(Main.isNumeric(input));
     }
 
-    @Test
-    void substituteCmd() {
-    }
-
-    @Test
-    void locateCmd() {
-    }
-
-    @Test
-    void deleteCmd() {
-    }
-
-    @Test
-    void moveCmd() {
-    }
-
-    @Test
-    void typeCmd() {
-    }
-
-    @Test
-    void pasteCmd() {
-    }
-
-    @Test
-    void insertCmd() {
-    }
-
-    @Test
-    void replaceCmd() {
-    }
-
-    @Test
-    void quitCmd() {
-    }
 }
